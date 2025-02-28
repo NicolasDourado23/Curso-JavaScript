b19b1f02fd8b8e1bc1a31e5bac8deafb344acdd5
Nicolas Dourado <nicolasramos.2301@gmail.com>
2025-02-28 18:06:29 -0300

    código da aula três

---
+++ aulas/aula-03.js
@@ -0,0 +1,20 @@
+ const idade = 28;
+ const nome = 'Nicolas';
+
+// if é igual a "se"
+
+// Quando se usar "||" e so um estiver verdadeiro passa, mas se os dois estiverem errados não passa.
+
+// Quando se usar "&&" e um dos dois estiverem errados não entra.
+
+//"=" Atribuição, "==" Contribuição, "===" Comparativo preciso.
+
+ if (idade >= 18 && nome === 'Nicolas'){
+    console.log('ENTROU!!!');
+} 
+ //e else é igual a "senao"
+
+ else {
+    console.log('NAO ENTRA!!!')
+ }
+
