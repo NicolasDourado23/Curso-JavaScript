d7b52028123d3e81aa79175076bcf317dd2b6013
Nicolas Dourado <nicolasramos.2301@gmail.com>
2025-03-05 17:21:04 -0300

    utilização do "for"

---
+++ aulas/aula-04.js
@@ -0,0 +1,12 @@
+//Como utilizar o for
+
+const quantidade = 10;
+let soma = 6;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(i);
+    soma = soma + i
+}
+
+console.log('Acabou!!!', soma)
+
